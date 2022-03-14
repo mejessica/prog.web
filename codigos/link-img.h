9261cf116e1de21a85d8e24a6955498085a37d87
Jéssica Gelsdorf <82670472+mejessica@users.noreply.github.com>
2022-03-14 17:43:56 -0300

    Create link-img.h

---
+++ codigos/link-img.h
@@ -0,0 +1,21 @@
+<!doctype html>
+<html> 
+    <head> 
+        <title>Título</title>
+        <meta charset="utf-8"/>
+    </head>
+
+    <body>
+        <blockquote> <!-- texto ou artigo de outro site ou autor-->
+              <p>
+                "Determinação, coragem e autoconfiança são fatores decisivos para o sucesso. Se estamos possuídos por uma inabalável determinação, conseguiremos superá-los. Independentemente das circunstâncias, devemos ser sempre humildes, recatados e despidos de orgulho.
+                
+              Dalai Lama 
+              </p>
+        </blockquote>
+
+        <a href="listas.html" target="_blank _self">Texto do link</a> <!--blank usado para abrir uma nova guia, self para abrir na mesma janela-->
+        <a name="">Texto do link</a> <!--name leva até uma parte do site, este uma vez sendo grande-->
+        
+    </body>
+</html>
